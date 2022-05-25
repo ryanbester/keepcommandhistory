940123089ebc5e66be78e291e9a732a8e8b4f6c3
Ryan Bester <34910027+ryanbester@users.noreply.github.com>
2022-05-25 20:34:35 +0100

    Update to support 1.18.2

--- src/main/java/com/ryanbester/keepcommandhistory/mixin/MixinNewChatGui.java
+++ src/main/java/com/ryanbester/keepcommandhistory/mixin/MixinNewChatGui.java
@@ -1,15 +1,15 @@
 package com.ryanbester.keepcommandhistory.mixin;
 
-import net.minecraft.client.gui.NewChatGui;
+import net.minecraft.client.gui.components.ChatComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(NewChatGui.class)
+@Mixin(ChatComponent.class)
 public abstract class MixinNewChatGui {
 
-    @Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/gui/NewChatGui;clearChatMessages(Z)V", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "clearMessages(Z)V", cancellable = true)
     private void onClearChatHistory(boolean variable, CallbackInfo callback) {
         if (variable) {
             callback.cancel();
